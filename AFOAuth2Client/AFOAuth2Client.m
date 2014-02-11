946a558ee8db676790ec957cbfb001d32c2d10f8
Mattt Thompson <m@mattt.me>
2014-02-11 09:13:37 -0800

    Defaulting expiration of tokens to distant future /cc @irlabs

--- AFOAuth2Client/AFOAuth2Client.m
+++ AFOAuth2Client/AFOAuth2Client.m
@@ -188,7 +188,7 @@ static NSMutableDictionary * AFKeychainQueryDictionaryWithIdentifier(NSString *i
 
         AFOAuthCredential *credential = [AFOAuthCredential credentialWithOAuthToken:[responseObject valueForKey:@"access_token"] tokenType:[responseObject valueForKey:@"token_type"]];
 
-        NSDate *expireDate = nil;
+        NSDate *expireDate = [NSDate distantFuture];
         id expiresIn = [responseObject valueForKey:@"expires_in"];
         if (expiresIn != nil && ![expiresIn isEqual:[NSNull null]]) {
             expireDate = [NSDate dateWithTimeIntervalSinceNow:[expiresIn doubleValue]];
