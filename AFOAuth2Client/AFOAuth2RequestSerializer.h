e988f4b320472255e3caee96a65302af8a0ba25f
Conrad Kramer <conrad@kramerapps.com>
2014-08-11 22:04:42 -0700

    Document AFOAuth2RequestSerializer

--- AFOAuth2Client/AFOAuth2RequestSerializer.h
+++ AFOAuth2Client/AFOAuth2RequestSerializer.h
@@ -12,6 +12,11 @@
 
 @interface AFOAuth2RequestSerializer : AFHTTPRequestSerializer
 
+/**
+ Sets the "Authorization" HTTP header set in request objects made by the HTTP client to contain the access token within the OAuth credential. This overwrites any existing value for this header.
+
+ @param credential The OAuth2 credential
+ */
 - (void)setAuthorizationHeaderFieldWithCredential:(AFOAuthCredential *)credential;
 
 @end
