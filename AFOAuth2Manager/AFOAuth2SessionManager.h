79bafa8baf84ca8f8fe47d9674d83aca5ce8dfcd
Eric Jensen <jensen39@gmail.com>
2015-09-06 14:42:34 -0400

    Add AFOAuth2SessionManager as equivalent to AFOAuth2RequestOperationManager that uses NSURLSession instead of NSURLConnection

---
+++ AFOAuth2Manager/AFOAuth2SessionManager.h
@@ -0,0 +1,190 @@
+// AFOAuth2SessionManager.h
+//
+// Copyright (c) 2012-2015 AFNetworking (http://afnetworking.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFHTTPSessionManager.h"
+#import "AFOAuthCredential.h"
+
+/**
+ `AFOAuth2Manager` encapsulates common patterns to authenticate against a resource server conforming to the behavior outlined in the OAuth 2.0 specification.
+
+ In your application, it is recommended that you use `AFOAuth2Manager` exclusively to get an authorization token, which is then passed to another `AFHTTPClient` subclass.
+
+ @see RFC 6749 The OAuth 2.0 Authorization Framework: http://tools.ietf.org/html/rfc6749
+ */
+@interface AFOAuth2SessionManager : AFHTTPSessionManager
+
+///------------------------------------------
+/// @name Accessing OAuth 2 Client Properties
+///------------------------------------------
+
+/**
+ The service provider identifier used to store and retrieve OAuth credentials by `AFOAuthCredential`. Equivalent to the hostname of the client `baseURL`.
+ */
+@property (readonly, nonatomic, copy) NSString *serviceProviderIdentifier;
+
+/**
+ The client identifier issued by the authorization server, uniquely representing the registration information provided by the client.
+ */
+@property (readonly, nonatomic, copy) NSString *clientID;
+
+/**
+ Whether to encode client credentials in a Base64-encoded HTTP `Authorization` header, as opposed to the request body. Defaults to `YES`.
+ */
+@property (nonatomic, assign) BOOL useHTTPBasicAuthentication;
+
+///------------------------------------------------
+/// @name Creating and Initializing OAuth 2 Clients
+///------------------------------------------------
+
+/**
+ Creates and initializes an `AFOAuth2SessionManager` object with the specified base URL, client identifier, and secret.
+
+ @param url The base URL for the HTTP client. This argument must not be `nil`.
+ @param clientID The client identifier issued by the authorization server, uniquely representing the registration information provided by the client. This argument must not be `nil`.
+ @param secret The client secret.
+
+ @return The newly-initialized OAuth 2 client
+ */
++ (instancetype)clientWithBaseURL:(NSURL *)url
+                         clientID:(NSString *)clientID
+                           secret:(NSString *)secret;
+
+/**
+ Creates and initializes an `AFOAuth2SessionManager` object with the specified base URL, client identifier, and secret.
+
+ @param url The base URL for the HTTP client. This argument must not be `nil`.
+ @param clientID The client identifier issued by the authorization server, uniquely representing the registration information provided by the client. This argument must not be `nil`.
+ @param secret The client secret.
+ @param configuration The configuration used to create the managed session.
+
+ @return The newly-initialized OAuth 2 client
+ */
++ (instancetype)clientWithBaseURL:(NSURL *)url
+                         clientID:(NSString *)clientID
+                           secret:(NSString *)secret
+             sessionConfiguration:(NSURLSessionConfiguration *)configuration;
+
+
+/**
+ Initializes an `AFOAuth2SessionManager` object with the specified base URL, client identifier, and secret. The communication to to the server will use HTTP basic auth by default (use `-(id)initWithBaseURL:clientID:secret:withBasicAuth:` to change this).
+
+ @param url The base URL for the HTTP client. This argument must not be `nil`.
+ @param clientID The client identifier issued by the authorization server, uniquely representing the registration information provided by the client. This argument must not be `nil`.
+ @param secret The client secret.
+
+ @return The newly-initialized OAuth 2 client
+ */
+- (instancetype)initWithBaseURL:(NSURL *)url
+                       clientID:(NSString *)clientID
+                         secret:(NSString *)secret;
+
+/**
+ Initializes an `AFOAuth2SessionManager` object with the specified base URL, client identifier, and secret. The communication to to the server will use HTTP basic auth by default (use `-(id)initWithBaseURL:clientID:secret:withBasicAuth:` to change this).
+
+ @param url The base URL for the HTTP client. This argument must not be `nil`.
+ @param clientID The client identifier issued by the authorization server, uniquely representing the registration information provided by the client. This argument must not be `nil`.
+ @param secret The client secret.
+ @param configuration The configuration used to create the managed session.
+
+ @return The newly-initialized OAuth 2 client
+ */
+- (instancetype)initWithBaseURL:(NSURL *)url
+                       clientID:(NSString *)clientID
+                         secret:(NSString *)secret
+           sessionConfiguration:(NSURLSessionConfiguration *)configuration;
+
+///---------------------
+/// @name Authenticating
+///---------------------
+
+/**
+ Creates and enqueues an `NSURLSessionDataTask` to authenticate against the server using a specified username and password, with a designated scope.
+
+ @param URLString The URL string used to create the request URL.
+ @param username The username used for authentication
+ @param password The password used for authentication
+ @param scope The authorization scope
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the OAuth credential returned by the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
+ */
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                     username:(NSString *)username
+                                                     password:(NSString *)password
+                                                        scope:(NSString *)scope
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure;
+
+/**
+ Creates and enqueues an `NSURLSessionDataTask` to authenticate against the server with a designated scope.
+
+ @param URLString The URL string used to create the request URL.
+ @param scope The authorization scope
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the OAuth credential returned by the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
+ */
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                        scope:(NSString *)scope
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure;
+
+/**
+ Creates and enqueues an `NSURLSessionDataTask` to authenticate against the server using the specified refresh token.
+
+ @param URLString The URL string used to create the request URL.
+ @param refreshToken The OAuth refresh token
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the OAuth credential returned by the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
+ */
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                 refreshToken:(NSString *)refreshToken
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure;
+
+/**
+ Creates and enqueues an `NSURLSessionDataTask` to authenticate against the server with an authorization code, redirecting to a specified URI upon successful authentication.
+
+ @param URLString The URL string used to create the request URL.
+ @param code The authorization code
+ @param uri The URI to redirect to after successful authentication
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the OAuth credential returned by the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
+ */
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                         code:(NSString *)code
+                                                  redirectURI:(NSString *)uri
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure;
+
+/**
+ Creates and enqueues an `NSURLSessionDataTask` to authenticate against the server with the specified parameters.
+
+ @param URLString The URL string used to create the request URL.
+ @param parameters The parameters to be encoded and set in the request HTTP body.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the OAuth credential returned by the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
+ */
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                   parameters:(NSDictionary *)parameters
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure;
+
+@end
---
+++ AFOAuth2Manager/AFOAuth2SessionManager.m
@@ -0,0 +1,234 @@
+// AFOAuth2SessionManager.m
+//
+// Copyright (c) 2012-2015 AFNetworking (http://afnetworking.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFOAuth2SessionManager.h"
+#import "AFOAuth2Constants.h"
+
+@interface AFOAuth2SessionManager ()
+@property (readwrite, nonatomic, copy) NSString *serviceProviderIdentifier;
+@property (readwrite, nonatomic, copy) NSString *clientID;
+@property (readwrite, nonatomic, copy) NSString *secret;
+@end
+
+@implementation AFOAuth2SessionManager
+
++ (instancetype)clientWithBaseURL:(NSURL *)url
+                         clientID:(NSString *)clientID
+                           secret:(NSString *)secret
+{
+    return [[self alloc] initWithBaseURL:url clientID:clientID secret:secret sessionConfiguration:nil];
+}
+
++ (instancetype)clientWithBaseURL:(NSURL *)url
+                         clientID:(NSString *)clientID
+                           secret:(NSString *)secret
+             sessionConfiguration:(NSURLSessionConfiguration *)configuration
+{
+    return [[self alloc] initWithBaseURL:url clientID:clientID secret:secret sessionConfiguration:configuration];
+}
+
+- (instancetype)initWithBaseURL:(NSURL *)url
+                       clientID:(NSString *)clientID
+                         secret:(NSString *)secret
+{
+    return [self initWithBaseURL:url clientID:clientID secret:secret sessionConfiguration:nil];
+}
+
+- (instancetype)initWithBaseURL:(NSURL *)url
+                       clientID:(NSString *)clientID
+                         secret:(NSString *)secret
+           sessionConfiguration:(NSURLSessionConfiguration *)configuration
+{
+    NSParameterAssert(clientID);
+
+    self = [super initWithBaseURL:url sessionConfiguration:configuration];
+    if (!self) {
+        return nil;
+    }
+
+    self.serviceProviderIdentifier = [self.baseURL host];
+    self.clientID = clientID;
+    self.secret = secret;
+
+    self.useHTTPBasicAuthentication = YES;
+
+    [self.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+
+    return self;
+}
+
+#pragma mark -
+
+- (void)setUseHTTPBasicAuthentication:(BOOL)useHTTPBasicAuthentication {
+    _useHTTPBasicAuthentication = useHTTPBasicAuthentication;
+
+    if (self.useHTTPBasicAuthentication) {
+        [self.requestSerializer setAuthorizationHeaderFieldWithUsername:self.clientID password:self.secret];
+    } else {
+        [self.requestSerializer setValue:nil forHTTPHeaderField:@"Authorization"];
+    }
+}
+
+- (void)setSecret:(NSString *)secret {
+    if (!secret) {
+        secret = @"";
+    }
+
+    _secret = secret;
+}
+
+#pragma mark -
+
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                     username:(NSString *)username
+                                                     password:(NSString *)password
+                                                        scope:(NSString *)scope
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure
+{
+    NSParameterAssert(username);
+    NSParameterAssert(password);
+    NSParameterAssert(scope);
+
+    NSDictionary *parameters = @{
+                                 @"grant_type": kAFOAuthPasswordCredentialsGrantType,
+                                 @"username": username,
+                                 @"password": password,
+                                 @"scope": scope
+                                 };
+
+    return [self authenticateUsingOAuthWithURLString:URLString parameters:parameters success:success failure:failure];
+}
+
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                        scope:(NSString *)scope
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure
+{
+    NSParameterAssert(scope);
+
+    NSDictionary *parameters = @{
+                                 @"grant_type": kAFOAuthClientCredentialsGrantType,
+                                 @"scope": scope
+                                 };
+
+    return [self authenticateUsingOAuthWithURLString:URLString parameters:parameters success:success failure:failure];
+}
+
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                 refreshToken:(NSString *)refreshToken
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure
+{
+    NSParameterAssert(refreshToken);
+
+    NSDictionary *parameters = @{
+                                 @"grant_type": kAFOAuthRefreshGrantType,
+                                 @"refresh_token": refreshToken
+                                 };
+
+    return [self authenticateUsingOAuthWithURLString:URLString parameters:parameters success:success failure:failure];
+}
+
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                         code:(NSString *)code
+                                                  redirectURI:(NSString *)uri
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure
+{
+    NSParameterAssert(code);
+    NSParameterAssert(uri);
+
+    NSDictionary *parameters = @{
+                                 @"grant_type": kAFOAuthCodeGrantType,
+                                 @"code": code,
+                                 @"redirect_uri": uri
+                                 };
+
+    return [self authenticateUsingOAuthWithURLString:URLString parameters:parameters success:success failure:failure];
+}
+
+- (NSURLSessionDataTask *)authenticateUsingOAuthWithURLString:(NSString *)URLString
+                                                   parameters:(NSDictionary *)parameters
+                                                      success:(void (^)(AFOAuthCredential *credential))success
+                                                      failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *mutableParameters = [NSMutableDictionary dictionaryWithDictionary:parameters];
+    if (!self.useHTTPBasicAuthentication) {
+        mutableParameters[@"client_id"] = self.clientID;
+        mutableParameters[@"client_secret"] = self.secret;
+    }
+    parameters = [NSDictionary dictionaryWithDictionary:mutableParameters];
+
+    NSURLSessionDataTask *task = [self POST:URLString parameters:parameters success:^(__unused NSURLSessionDataTask *task, id responseObject) {
+        if (!responseObject) {
+            if (failure) {
+                failure(nil);
+            }
+
+            return;
+        }
+
+        if ([responseObject valueForKey:@"error"]) {
+            if (failure) {
+                failure(AFErrorFromRFC6749Section5_2Error(responseObject));
+            }
+
+            return;
+        }
+
+        NSString *refreshToken = [responseObject valueForKey:@"refresh_token"];
+        if (!refreshToken || [refreshToken isEqual:[NSNull null]]) {
+            refreshToken = [parameters valueForKey:@"refresh_token"];
+        }
+
+        AFOAuthCredential *credential = [AFOAuthCredential credentialWithOAuthToken:[responseObject valueForKey:@"access_token"] tokenType:[responseObject valueForKey:@"token_type"]];
+
+
+        if (refreshToken) { // refreshToken is optional in the OAuth2 spec
+            [credential setRefreshToken:refreshToken];
+        }
+
+        // Expiration is optional, but recommended in the OAuth2 spec. It not provide, assume distantFuture === never expires
+        NSDate *expireDate = [NSDate distantFuture];
+        id expiresIn = [responseObject valueForKey:@"expires_in"];
+        if (expiresIn && ![expiresIn isEqual:[NSNull null]]) {
+            expireDate = [NSDate dateWithTimeIntervalSinceNow:[expiresIn doubleValue]];
+        }
+
+        if (expireDate) {
+            [credential setExpiration:expireDate];
+        }
+        
+        if (success) {
+            success(credential);
+        }
+    } failure:^(__unused NSURLSessionDataTask *task, NSError *error) {
+        if (failure) {
+            failure(error);
+        }
+    }];
+    
+    return task;
+}
+
+@end
