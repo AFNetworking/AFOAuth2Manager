10f7e27bbe2d02b0732aeb39aa31205d6387bdfa
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-10-28 10:11:54 -0500

    basic project setup

---
+++ AFOAuth2Manager/AFOAuth2SessionManager.h
@@ -0,0 +1,14 @@
+//
+//  AFOAuth2SessionManager.h
+//  AFOAuth2Manager
+//
+//  Created by Kevin Harwood on 10/28/15.
+//  Copyright © 2015 Alamofire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import AFNetworking;
+
+@interface AFOAuth2SessionManager : AFHTTPSessionManager
+
+@end
---
+++ AFOAuth2Manager/AFOAuth2SessionManager.m
@@ -0,0 +1,13 @@
+//
+//  AFOAuth2SessionManager.m
+//  AFOAuth2Manager
+//
+//  Created by Kevin Harwood on 10/28/15.
+//  Copyright © 2015 Alamofire. All rights reserved.
+//
+
+#import "AFOAuth2SessionManager.h"
+
+@implementation AFOAuth2SessionManager
+
+@end
