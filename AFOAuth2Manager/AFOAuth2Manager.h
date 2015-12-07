7257662b3c571beb302d7d50032453e6f7473b91
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-12-07 13:17:33 -0600

    Fixed Foundation import

--- AFOAuth2Manager/AFOAuth2Manager.h
+++ AFOAuth2Manager/AFOAuth2Manager.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 @class AFOAuthCredential;
 @import AFNetworking;
 
